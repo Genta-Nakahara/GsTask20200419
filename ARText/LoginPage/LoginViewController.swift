57377f384bc6fd6ef72366524ff24d6bc79542a0
genta.nakahara <genta.nakahara@gmail.com>
2020-04-19 00:34:01 +0900

    no message

---
+++ ARText/LoginPage/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  ARText
+//
+//  Created by hdymacuser on 2020/04/18.
+//  Copyright © 2020 Mark Zhong. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func tapSignButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func tapLoginButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func tapTermsButton(_ sender: Any) {
+        
+      
+        
+    }
+    
+    
+    
+}
